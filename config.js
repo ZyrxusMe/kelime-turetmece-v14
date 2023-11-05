98769862a7f16515a39a20411ce63b2cc1fe957d
ZyrxusMe <ardapaydnn@gmail.com>
2023-11-05 12:12:20 +0300

    data

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
+
 export default {
-<<<<<<< HEAD
     "token": "YOUR_BOT_TOKEN_HERE",
-=======
-    "token": "YOUR_BOT_TOKEN_HERE",
->>>>>>> 5b7ad1b20de53e747629ef2d6327fc210c1950b9
     "activity": "Hello World"
 }
